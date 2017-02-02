79f24e9d45d05d1d2077d5fc120bab2591a69ad7
ASUS-MAXIV\Jorge <rafaeljorge38@yahoo.com>
2017-02-01 23:47:56 -0800

    newCharacterControllerByWei
    Collision still ineffective

---
+++ Assets/ScriptLibrary/BuildingScripts/BuildingHealth.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingHealth : MonoBehaviour {
+
+    public float healthAmount = 100;
+    public BuildingAtributes DB;
+    void Start()
+    {
+        DB = gameObject.GetComponent<BuildingAtributes>();
+        //Debug.Log(DB.name);
+        if (DB == null)
+        {
+            Debug.LogError(gameObject.name);
+            Debug.LogError(": does not have a DistructBuilding Script");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            DB.MinusHealth(healthAmount);
+            //Debug.Log(DB.name);
+            if (DB.BuildingHealth <= 0)
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
